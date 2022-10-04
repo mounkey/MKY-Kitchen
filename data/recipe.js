f07857f1fb71f2e76559c8baf32843b54e7b4d6f
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jpgrover@mounkey.cl>
2022-10-04 19:39:52 -0300

    add folder data & i preparate add data folder & prepare to use Redux in the app

---
+++ data/recipe.js
@@ -0,0 +1,162 @@
+export const recipe = [
+  {
+    "id": 1,
+    "name": "Pasta",
+    "description": "Pasta con salsa de tomate",
+    "image": "https://www.recetasderechupete.com/wp-content/uploads/2019/03/pasta-con-salsa-de-tomate.jpg",
+    "ingredients": [
+      {
+        "id": 1,
+        "id_recipe": 1,
+        "name": "Harina",
+        "quantity": 1,
+        "unit": "kg"
+      },
+      {
+        "id": 2,
+        "id_recipe": 1,
+        "name": "Huevos",
+        "quantity": 2,
+        "unit": "unidades"
+      },
+      {
+        "id": 3,
+        "id_recipe": 1,
+        "name": "Aceite de oliva",
+        "quantity": 1,
+        "unit": "cucharada"
+      },
+      {
+        "id": 4,
+        "id_recipe": 1,
+        "name": "Sal",
+        "quantity": 1,
+        "unit": "cucharada"
+      },
+      {
+        "id": 5,
+        "id_recipe": 1,
+        "name": "Tomate",
+        "quantity": 1,
+        "unit": "kg"
+      }, 
+      {
+        "id": 6,
+        "id_recipe": 1,
+        "name": "Ajo",
+        "quantity": 2,
+        "unit": "dientes"
+      },
+      {
+        "id": 7,
+        "id_recipe": 1,
+        "name": "Perijil",
+        "quantity": 1,
+        "unit": "ramita"
+      },
+    ],
+    "steps": [
+      {
+        "id": 1,
+        "id_recipe": 1,
+        "step": "Mezclar harina y huevos, amazarlo y pasarlo por el rodillo hasta llener una masa homogenea y la consistencia que se desee"
+      },
+      {
+        "id": 2,
+        "id_recipe": 1,
+        "step": "Cortar la masa en tiras y cocinar en agua hirviendo tiempo de cocinado 5 minutos"
+      },
+    ],
+    "other": [
+      {
+        "id": 1,
+        "id_recipe": 1,
+        "name": "salsa de tomate",
+        "step": "Corte los tomate y coloque perejil a gusto sal y ajo y cocine por 10 minutos"      
+
+      }
+    ],
+    "photo": [
+      {
+        "id": 1,
+        "id_recipe": 1,
+        "image": "https://www.recetasderechupete.com/wp-content/uploads/2019/03/pasta-con-salsa-de-tomate.jpg"
+      },
+      {
+        "id": 2,
+        "id_recipe": 1, 
+        "image": "https://www.recetasderechupete.com/wp-content/uploads/2019/03/pasta-con-salsa-de-tomate.jpg"
+      },
+    ],
+  },
+  {
+    "id": 2,
+    "name": "Pizza",
+    "description": "Pizza con salsa de tomate",
+    "image": "https://t2.rg.ltmcdn.com/es/posts/1/9/3/pizza_casera_31391_orig.jpg",
+
+    "ingredients": [
+      {
+        "id": 8,
+        "id_recipe": 2,
+        "name": "Harina de trigó",
+        "quantity": 0.4,
+        "unit": "kg"
+      },
+      {
+        "id": 9,
+        "id_recipe": 2,
+        "name": "Levadura",
+        "quantity": 1,
+        "unit": "cucharada"
+      },
+      {
+        "id": 10,
+        "id_recipe": 2,
+        "name": "Sal",
+        "quantity": 0.5,
+        "unit": "cucharada"
+      },
+      {
+        "id": 11,
+        "id_recipe": 2,
+        "name": "Aceite de oliva",
+        "quantity": 1,
+        "unit": "cucharada"
+      },
+      {
+        "id": 12,
+        "id_recipe": 2,
+        "name": "agua",
+        "quantity": 1,
+        "unit": "taza"
+      },
+    ],
+    "steps": [
+      { 
+        "id": 3,
+        "id_recipe": 2,
+        "step": "Mezclar harina, levadura, sal y aceite de oliva, amasar y dejar reposar por 30 minutos"
+      },
+      {
+        "id": 4,
+        "id_recipe": 2,
+        "step": "Estirar la masa y colocar los ingredientes que se desee"
+      },
+      {
+        "id": 5,
+        "id_recipe": 2,
+        "step": "Cocinar en horno a 180 grados por 20 minutos"
+      },
+    ],
+    "other": [
+    ],
+    "photo": [
+      {
+        "id": 3,
+        "id_recipe": 2,
+        "image": "https://t2.rg.ltmcdn.com/es/posts/1/9/3/pizza_casera_31391_orig.jpg"
+      },
+    ],
+  }
+]
