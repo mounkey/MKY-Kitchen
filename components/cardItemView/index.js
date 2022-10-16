ddf7f7a2beacec1c03568d1d9967a98b612f334e
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jpgrover@mounkey.cl>
2022-10-16 16:50:33 -0300

    no Resolver

--- components/cardItemView/index.js
+++ components/cardItemView/index.js
@@ -5,18 +5,15 @@ import {styles} from "./style";
 
 const CardItemView = ({ item, addStatus }) => {   
 
-  const [status, setStatus] = useState(true);
-  const handlerStatus = () => {
-    setStatus(!status);
-    addStatus(item.id);
-  }
+ 
+
 
   return (
     item.map((items) => (
     <View style={styles.itemContainer} key={items.id}>
       <Text style={ items.status == false ? styles.itemText : styles.itemTextTachado}>{items.product + ' - ' + items.quantity + ' - ' + items.measure}</Text>
       <View style={styles.itemButtons}>
-        <TouchableOpacity  onPress= {() => addStatus(items.id, status)}  onPressOut = {handlerStatus()}>
+        <TouchableOpacity  onPress= {() => addStatus(items.id)} >
           <Text style = { styles.itemTextButton} >Tachar</Text>          
         </TouchableOpacity>
       </View>
