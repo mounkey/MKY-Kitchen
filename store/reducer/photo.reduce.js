4143ad63c88f563b894a60134464d226cc44ff1f
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jpgrover@mounkey.cl>
2022-10-24 15:29:38 -0300

    sigue el error

--- store/reducer/photo.reduce.js
+++ store/reducer/photo.reduce.js
@@ -30,4 +30,42 @@ const photoReducer = (state = initialState, action) => {
   }
 }
 
+export const fetchPhotos = () => {
+  return async (dispatch) => {
+    try {
+      const dbResult = await fetchImages();
+      dispatch({ type: SET_PHOTOS, photos: dbResult.rows._array });
+    } catch (err) {
+      throw err;
+    }
+  };
+};
+
+export const savePlace = (image) => {
+  return async (dispatch) => {
+    const response = await fetch(image);
+    if (!response.ok) {
+      throw new Error("Something went wrong!");
+    }
+    const data = await response.jason();
+    if(!data.result){
+      throw new Error("Something went wrong!");
+    }
+    //const fileName = data.result.split("/").pop();
+    //const newPath = FileSystem.documentDirectory + fileName;
+    try {
+      //await FileSystem.moveAsync({
+        //from: image,
+        //to: newPath,
+      //});
+      const dbResult = await insertImage(id, image);
+      dispatch(ADD_PHOTO({ id: dbResult.insertId, imagen }));
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  };
+};
+
+
 export default photoReducer;
