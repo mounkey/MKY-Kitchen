0455b71b543d185ddf710933cc6dceaf9ae4bb48
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jpgrover@mounkey.cl>
2022-11-06 13:37:09 -0300

    Ready upload & delete list

--- store/action/list.action.js
+++ store/action/list.action.js
@@ -1,33 +1,117 @@
+import {URL_API} from '../../firebase';
 import {listTypes} from '../types';
 
 const { ADD_LIST, DELETE_LIST, SELECT_LIST, CHANGE_PRODUCT_STATUS, REMOVE_PRODUCT_LIST } = listTypes;
 
 export const addlist= (list, idList, nameList, product, quantity, measure, status) => {
-  return {
-    type: ADD_LIST,
-    list,
-    idList,
-    nameList,
-    product,
-    quantity,
-    measure,
-    status,
+  return async (dispatch) => {
+    try {
+      const response = await fetch(`${URL_API}/lists.json`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          listname: nameList,
+          products: [
+            {
+              id: Date.now().toString(),
+              idlist: idList,
+              product: product,
+              quantity: quantity,
+              measure: measure,
+              status: status,
+            },
+          ],
+        }),
+      });
+      const resData = await response.json();
+      dispatch({
+        type: ADD_LIST,
+        list,
+        idList,
+        nameList,
+        product,
+        quantity,
+        measure,
+        status
+      });
+    } catch (err) {
+      throw err;
+    }
   };
-}
+};
 
 export const removeList = (id) => {
-  return {
-    type: DELETE_LIST,
-    id: id,
+  return async(dispatch) =>{
+    try {
+      const response = await fetch(`${URL_API}/list/${id}.json`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+
+      const data = await response.json();
+      dispatch({
+        type: DELETE_LIST,
+        id: id,
+      });
+    } catch (error) {
+      console.log(error);
+    }   
   };
 }
 
-export const showList = (id) => {
-  return {
-    type: SELECT_LIST,
-    listId: id,
-  };
+export const selectList = () => {
+  return async (dispatch) => {
+    try {
+      const response = await fetch(`${URL_API}/list.json`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+
+      const data = await response.json();
+      const test = Object.keys(data).map(key => data[key]);
+      const list = test;
+      
+      dispatch({
+        type: SELECT_LIST,
+        list: list,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
+
+
+export const showList = (id) => {
+  return async(dispatch) => {
+    try {
+      const response = await fetch(`${URL_API}/list/${id}.json`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+
+      const data = await response.json();
+      const test = Object.keys(data).map(key =>data[key]);
+      const list = test.filter(item => item.id === id);
+
+
+      dispatch({
+        type: SELECT_LIST,
+        list: list,
+      });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
 
 export const changeProductStatus = (id) => {
   return {
